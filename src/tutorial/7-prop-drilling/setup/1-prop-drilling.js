257c1488151def66ab0c5566ee1283204bb90bab
Knight <mayanks365@gmail.com>
2021-02-19 23:44:00 +0530

    updating prop-drilling and context files

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,45 @@
 import React, { useState } from 'react';
-
+import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
-};
+  const [ people, setPeople ] = useState(data);
+
+  const removePerson = (id) => {
+    setPeople( (people) => (
+      people.filter( (person) => person.id !== id )
+    ));
+  }
+
+  const SinglePerson = ( { id, name, removePerson } ) => {
+    return (
+      <div className="item">
+        <h4>{name}</h4>
+        <button onClick={ () => removePerson(id) }>Remove</button>
+      </div>
+    )
+  }
+
+  const List = ( { people, removePerson } ) => {
+    return (
+      <>
+        {people.map( ( person ) => (
+          <SinglePerson 
+            key={person.id} 
+            {...person}
+            removePerson={removePerson}/>
+        ))}
+      </>
+    )
+  };
+
+  return (
+    <section>
+      <h3>Prop Drilling</h3>
+      <List people = {people} removePerson= {removePerson}/>
+    </section>
+  );
+}
 
 export default PropDrilling;
--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,22 +3,27 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext();
+// two component - Provider, Consumer
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
+
   const removePerson = (id) => {
     setPeople((people) => {
       return people.filter((person) => person.id !== id);
     });
   };
+
   return (
-    <>
+    <PersonContext.Provider value={ {removePerson, nam:'amana'} }>
       <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+      <List people={people}  />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
@@ -26,7 +31,6 @@ const List = ({ people, removePerson }) => {
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +38,9 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext);
+
   return (
     <div className='item'>
       <h4>{name}</h4>
