7360ecb4b39775ae28ce4d07086cd83250da506f
Knight <mayanks365@gmail.com>
2021-03-08 10:05:01 +0530

    updating Router files

--- src/tutorial/11-react-router/setup/People.js
+++ src/tutorial/11-react-router/setup/People.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { data } from '../../../data';
 import { Link } from 'react-router-dom';
+
 const People = () => {
   const [people, setPeople] = useState(data);
   return (
@@ -10,6 +11,7 @@ const People = () => {
         return (
           <div key={person.id} className='item'>
             <h4>{person.name}</h4>
+            <Link to={`/person/${person.id}`}>Learn More</Link>
           </div>
         );
       })}
--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
+
 const Person = () => {
-  return (
+  const [ name, setName ] = useState('default name')
+   const { id } = useParams(); // id will be string
+
+  // code is for name, can ignore
+  useEffect( () => {
+    const newPerson = data.find( (person) =>
+      person.id === parseInt(id)
+    )
+    // console.log(newPerson)
+    setName(newPerson.name)
+  },[])
+
+   return (
     <div>
-      <h2>person</h2>
+      <h1>{name}</h1>
+      <h3>{id}</h3>
+      <Link to='/people' className='btn'>Back To People</Link>
     </div>
   );
 };
